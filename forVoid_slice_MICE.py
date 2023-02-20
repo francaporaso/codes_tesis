1dfe57fcfc6a5591bc03ad0cbda9b06e7a360f63
Franco <fmcaporaso@mi.unc.edu.ar>
2023-02-20 13:34:11 -0300

    update forVoid_profile, añado forVoid_slice para sacar el profile por partes

--- forVoid_slice_MICE.py
+++ forVoid_slice_MICE.py
@@ -1,3 +1,4 @@
+'''for small cuts in Rv or z, to be used with a forVoid_paste to paste profiles'''
 import sys
 sys.path.append('../lens_codes_v3.7')
 import time
@@ -140,118 +141,6 @@ def SigmaCrit(zl, zs, h=1.):
     return (((cvel**2.0)/(4.0*np.pi*G*Dl))*(1./BETA_array))*(pc**2/Msun)
 
 
-def partial_map(RA0,DEC0,Z,Rv,
-                RIN,ROUT,ndots,h,
-                addnoise):
-
-        
-        lsize = int(np.sqrt(ndots))
-        ndots = int(ndots)
-        
-        cosmo = LambdaCDM(H0=100*h, Om0=0.25, Ode0=0.75)
-        dl  = cosmo.angular_diameter_distance(Z).value
-        KPCSCALE   = dl*(((1.0/3600.0)*np.pi)/180.0)*1000.0
-        
-        delta = (ROUT*1.5)/(3600*KPCSCALE)
-        rarray = [[-1.*(ROUT*1.e-3),(ROUT*1.e-3)],[-1.*(ROUT*1.e-3),(ROUT*1.e-3)]]
-        
-        t0 = time.time()
-        # mask = (S.ra < (RA0+delta))&(S.ra > (RA0-delta))&(S.dec > (DEC0-delta))&(S.dec < (DEC0+delta))&(S.z_v > (Z+0.1))
-        mask = (S.ra_gal < (RA0+delta))&(S.ra_gal > (RA0-delta))&(S.dec_gal > (DEC0-delta))&(S.dec_gal < (DEC0+delta))&(S.z_cgal_v > (Z+0.1))
-                       
-        catdata = S[mask]
-
-        sigma_c = SigmaCrit(Z, catdata.z_cgal_v)
-
-        # Add miscentring
-        c1 = SkyCoord(RA0*u.degree, DEC0*u.degree)
-        roff_deg = roff/(KPCSCALE*3600.)
-        c2 = c1.directional_offset_by(phi_off*u.degree,roff_deg*u.degree)
-
-
-        rads, theta, test1,test2 = eq2p2(np.deg2rad(catdata.ra_gal),
-                                        np.deg2rad(catdata.dec_gal),
-                                        np.deg2rad(c2.ra.value),
-                                        np.deg2rad(c2.dec.value))
-
-
-        theta2 = (2.*np.pi - theta) +np.pi/2.
-        theta_ra = theta2
-        theta_ra[theta2 > 2.*np.pi] = theta2[theta2 > 2.*np.pi] - 2.*np.pi
-                       
-        e1     = catdata.gamma1
-        e2     = -1.*catdata.gamma2
-        
-        # Add shape noise due to intrisic galaxy shapes        
-        if addnoise:
-            es1 = -1.*catdata.eps1
-            es2 = catdata.eps2
-            e1 += es1
-            e2 += es2
-       
-        #get tangential ellipticities 
-        et = (-e1*np.cos(2*theta)-e2*np.sin(2*theta))*sigma_c
-        #get cross ellipticities
-        ex = (-e1*np.sin(2*theta)+e2*np.cos(2*theta))*sigma_c
-        # '''
-        k  = catdata.kappa*sigma_c
-                
-        wcs.wcs.crval = [RA0,DEC0]
-        dx, dy = wcs.wcs_world2pix(catdata.ra,catdata.dec, 0)
-
-        dx = dx*KPCSCALE*1.e-3
-        dy = dy*KPCSCALE*1.e-3
-          
-        del(e1)
-        del(e2)
-        
-        r = np.rad2deg(rads)*3600*KPCSCALE
-        
-        del(rads)
-        
-        
-        Ntot = len(catdata)
-        del(catdata)    
-        
-        
-        t = np.tile(theta_ra,(3,1)).T
-        an = np.tile(angles,(len(theta_ra),1))
-        at     = t - an
-        
-        GTsum = np.zeros((ndots,3))
-        GXsum = np.zeros((ndots,3))
-        Ksum  = np.zeros((ndots,3))
-        Ninbin  = np.zeros((ndots,3))
-        
-        
-        for j in range(3):
-                x_t  = (dx*np.cos(an[:,j]) + dy*np.sin(an[:,j])) 
-                y_t = (-1.*dx*np.sin(an[:,j]) + dy*np.cos(an[:,j])) 
-                m   =  (abs(x_t) < (ROUT*1.e-3))*(abs(y_t) < (ROUT*1.e-3)) 
-                out = stats.binned_statistic_2d(x_t[m], y_t[m], et[m], 
-                                               'count',bins=lsize, range = rarray)
-                Ninbin[:,j] = out.statistic.flatten()
-                out = stats.binned_statistic_2d(x_t[m], y_t[m], et[m], 
-                                               'sum',bins=lsize, range = rarray)
-                GTsum[:,j] = out.statistic.flatten()
-                out = stats.binned_statistic_2d(x_t[m], y_t[m], ex[m], 
-                                               'sum',bins=lsize, range = rarray)
-                GXsum[:,j] = out.statistic.flatten()
-                out = stats.binned_statistic_2d(x_t[m], y_t[m], k[m], 
-                                               'sum',bins=lsize, range = rarray)
-                Ksum[:,j] = out.statistic.flatten()
-
-        Ntot = m.sum()
-   
-        output = {'GTsum':GTsum,'GXsum':GXsum,
-                   'Ksum': Ksum, 'N_inbin':Ninbin,
-                   'Ntot': Ntot}
-        
-        return output
-
-def partial_map_unpack(minput):
-	return partial_map(*minput)
-
 def partial_profile(RA0,DEC0,Z,Rv,
                     RIN,ROUT,ndots,h,
                     addnoise):
@@ -291,6 +180,7 @@ def partial_profile(RA0,DEC0,Z,Rv,
         et = (-e1*np.cos(2*theta)-e2*np.sin(2*theta))*sigma_c
         #get cross ellipticities
         ex = (-e1*np.sin(2*theta)+e2*np.cos(2*theta))*sigma_c
+               
         #get convergence
         k  = catdata.kappa*sigma_c
           
@@ -325,20 +215,6 @@ def partial_profile(RA0,DEC0,Z,Rv,
 def partial_profile_unpack(minput):
 	return partial_profile(*minput)
 
-def cov_matrix(array):
-        
-        K = len(array)
-        Kmean = np.average(array,axis=0)
-        bins = array.shape[1]
-        
-        COV = np.zeros((bins,bins))
-        
-        for k in range(K):
-            dif = (array[k]- Kmean)
-            COV += np.outer(dif,dif)        
-        
-        COV *= (K-1)/K
-        return COV
         
 def main(lcat, sample='pru',
          Rv_min=0.,Rv_max=50.,
@@ -419,19 +295,7 @@ def main(lcat, sample='pru',
                 ides = np.loadtxt(idlist).astype(int)
                 mvoids = np.in1d(L[0],ides)
         else:                
-                mradio  = (Rv >= Rv_min)&(Rv < Rv_max)
-                mz      = (z >= z_min)&(z < z_max)
-                mrho1   = (rho_1 >= rho1_min)&(rho_1 < rho2_max)
-                mrho2   = (rho_2 >= rho2_min)&(rho_2 < rho2_max)
-                mflag   = flag >= FLAG
-                mvoids = mradio & mz & mrho1 & mrho2 & mflag
-
-                del mradio
-                del mz
-                del mrho1
-                del mrho2
-                del mflag
-        
+                mvoids = ((Rv >= Rv_min)&(Rv < Rv_max))&((z >= z_min)&(z < z_max))&((rho_1 >= rho1_min)&(rho_1 < rho2_max))&((rho_2 >= rho2_min)&(rho_2 < rho2_max))&(flag >= FLAG)        
         # SELECT RELAXED HALOS
                 
         Nvoids = mvoids.sum()
@@ -451,8 +315,7 @@ def main(lcat, sample='pru',
         kmask[0] = np.ones(len(ra)).astype(bool)
         
         ramin  = np.min(ra)
-        cdec   = np.sin(np.deg2rad(dec))
-        decmin = np.min(cdec)
+        decmin = np.min(np.sin(np.deg2rad(dec)))
         dra    = ((np.max(ra)+1.e-5)  - ramin)/10.
         ddec   = ((np.max(cdec)+1.e-5) - decmin)/10.
         
@@ -482,38 +345,7 @@ def main(lcat, sample='pru',
         del L
 
         if domap:
-
-            print('Maps have ',ndots,'pixels')
-            print('up to ',ROUT,'kpc')
-            
-            os.system('mkdir ../maps')
-            output_file = 'maps/map_'+sample+'.fits'
-
-            # Defining 2D bins
-            
-            lsize = int(np.sqrt(ndots))
-            
-            ndots = int(lsize**2)
-            
-            Rmpc = 1.e-3*ROUT
-            
-            xb = np.linspace(-1.*Rmpc+(Rmpc/lsize),Rmpc-(Rmpc/lsize),lsize)
-            
-            ybin, xbin = np.meshgrid(xb,xb)
-            
-            ybin = ybin.flatten()
-            xbin = xbin.flatten()
-
-            # WHERE THE SUMS ARE GOING TO BE SAVED
-            GTsum = np.zeros((ncen+1,ndots,3))
-            GXsum = np.zeros((ncen+1,ndots,3))
-            Ksum  = np.zeros((ncen+1,ndots,3))        
-            
-            Ninbin = np.zeros((ncen+1,ndots,3))
-            
-            # FUNCTION TO RUN IN PARALLEL
-            partial = partial_map_unpack
-            
+                print('Sin mapa')           
 
         else:
 
@@ -583,13 +415,7 @@ def main(lcat, sample='pru',
                         Ntot[j] = profilesums['Ntot']
                         
                         if domap:
-                            
-                            km      = np.tile(Ksplit[l][j],(3,ndots,1)).T
-                            Ninbin += np.tile(profilesums['N_inbin'],(ncen+1,1,1))*km
-
-                            GTsum += np.tile(profilesums['GTsum'],(ncen+1,1,1))*km
-                            GXsum += np.tile(profilesums['GXsum'],(ncen+1,1,1))*km
-                            Ksum  += np.tile(profilesums['Ksum'],(ncen+1,1,1))*km
+                                print('Sin mapa')
                             
                         else:
 
@@ -613,7 +439,7 @@ def main(lcat, sample='pru',
 
         h = fits.Header()
         h.append(('N_VOIDS',np.int(Nvoids)))
-        h.append(('Lens cat',lcat))
+        h.append(('Lens_cat',lcat))
         #h.append(('MICE version sources 2.0'))
         h.append(('Rv_min',np.round(Rv_min,2)))
         h.append(('Rv_max',np.round(Rv_max,2)))
@@ -626,90 +452,48 @@ def main(lcat, sample='pru',
         h.append(('hcosmo',np.round(hcosmo,4)))
 
         if domap:
-            
-            # COMPUTING PROFILE        
-                
-            GT  = (GTsum/Ninbin)
-            GX  = (GXsum/Ninbin)
-            K   = (Ksum/Ninbin)
-                    
-            # COMPUTE COVARIANCE
-            
-            COV_Gtc = cov_matrix(GT[1:,:,0])
-            COV_Gt  = cov_matrix(GT[1:,:,1])
-            COV_Gtr = cov_matrix(GT[1:,:,2])
-            
-            COV_Gxc = cov_matrix(GX[1:,:,0])
-            COV_Gx  = cov_matrix(GX[1:,:,1])
-            COV_Gxr = cov_matrix(GX[1:,:,2])
-    
-            COV_Kc = cov_matrix(GX[1:,:,0])
-            COV_K  = cov_matrix(GX[1:,:,1])
-            COV_Kr = cov_matrix(GX[1:,:,2])
-            
-            table_pro = [fits.Column(name='xmpc', format='E', array=xbin),
-                    fits.Column(name='ympc', format='E', array=ybin),
-                    fits.Column(name='GT_control', format='E', array=GT[0,:,0]),
-                    fits.Column(name='GT', format='E', array=GT[0,:,1]),
-                    fits.Column(name='GT_reduced', format='E', array=GT[0,:,2]),
-                    fits.Column(name='GX_control', format='E', array=GX[0,:,0]),
-                    fits.Column(name='GX', format='E', array=GX[0,:,1]),
-                    fits.Column(name='GX_reduced', format='E', array=GX[0,:,2]),
-                    fits.Column(name='K_control', format='E', array=K[0,:,0]),
-                    fits.Column(name='K', format='E', array=K[0,:,1]),
-                    fits.Column(name='K_reduced', format='E', array=K[0,:,2])]
-                    
-                        
-            table_cov = [fits.Column(name='COV_GT_control', format='E', array=COV_Gtc.flatten()),
-                        fits.Column(name='COV_GT', format='E', array=COV_Gt.flatten()),
-                        fits.Column(name='COV_GT_reduced', format='E', array=COV_Gtr.flatten()),
-                        fits.Column(name='COV_GX_control', format='E', array=COV_Gxc.flatten()),
-                        fits.Column(name='COV_GX', format='E', array=COV_Gx.flatten()),
-                        fits.Column(name='COV_GX_reduced', format='E', array=COV_Gxr.flatten()),
-                        fits.Column(name='COV_K_control', format='E', array=COV_Kc.flatten()),
-                        fits.Column(name='COV_K', format='E', array=COV_K.flatten()),
-                        fits.Column(name='COV_K_reduced', format='E', array=COV_Kr.flatten())]
-    
-    
-        else:
-            # COMPUTING PROFILE        
-            Ninbin[DSIGMAwsum_T == 0] = 1.
-                    
-            Sigma     = (SIGMAwsum/Ninbin)
-            DSigma_T  = (DSIGMAwsum_T/Ninbin)
-            DSigma_X  = (DSIGMAwsum_X/Ninbin)
-            
-            
-            # COMPUTE COVARIANCE
-    
-            COV_S   = cov_matrix(Sigma[1:,:])
-            COV_DSt  = cov_matrix(DSigma_T[1:,:])
-            COV_DSx  = cov_matrix(DSigma_X[1:,:])
-
-            # WRITING OUTPUT FITS FILE
-            
-            table_pro = [fits.Column(name='Rp', format='E', array=R),
-                    fits.Column(name='Sigma', format='E', array=Sigma[0]),
-                    fits.Column(name='DSigma_T', format='E', array=DSigma_T[0]),
-                    fits.Column(name='DSigma_X', format='E', array=DSigma_X[0]),
-                    fits.Column(name='Ninbin', format='E', array=Ninbin[0])]
-                    
-                        
-            table_cov = [fits.Column(name='COV_DST', format='E', array=COV_DSt.flatten()),
-                        fits.Column(name='COV_S', format='E', array=COV_S.flatten()),
-                        fits.Column(name='COV_DSX', format='E', array=COV_DSx.flatten())]
+                print('Sin mapa')         
         
-            #table_cov = [fits.Column(name='Sigma', format='E', array=Sigma.flatten()),
-            #            fits.Column(name='DSigma_T', format='E', array=DSigma_T.flatten()),
-            #            fits.Column(name='DSIgma_X', format='E', array=DSIgma_X.flatten())]
+        else:
+                # COMPUTING PROFILE        
+                Ninbin[DSIGMAwsum_T == 0] = 1.
+                        
+                Sigma     = (SIGMAwsum/Ninbin)
+                DSigma_T  = (DSIGMAwsum_T/Ninbin)
+                DSigma_X  = (DSIGMAwsum_X/Ninbin)
+                
+                
+                # COMPUTE COVARIANCE
+        
+                # COV_S   = cov_matrix(Sigma[1:,:])
+                # COV_DSt  = cov_matrix(DSigma_T[1:,:])
+                # COV_DSx  = cov_matrix(DSigma_X[1:,:])
+    
+                # WRITING OUTPUT FITS FILE
+                
+                """table_pro = [fits.Column(name='Rp', format='E', array=R),
+                        fits.Column(name='Sigma', format='E', array=Sigma[0]),
+                        fits.Column(name='DSigma_T', format='E', array=DSigma_T[0]),
+                        fits.Column(name='DSigma_X', format='E', array=DSigma_X[0]),
+                        fits.Column(name='Ninbin', format='E', array=Ninbin[0])]
+                        
+                            
+                table_cov = [fits.Column(name='COV_DST', format='E', array=COV_DSt.flatten()),
+                            fits.Column(name='COV_S', format='E', array=COV_S.flatten()),
+                            fits.Column(name='COV_DSX', format='E', array=COV_DSx.flatten())] """
+            
+                table_p = [fits.Column(name='Sigma',    format='E', array=Sigma.flatten()),
+                           fits.Column(name='DSigma_T', format='E', array=DSigma_T.flatten()),
+                           fits.Column(name='DSIgma_X', format='E', array=DSIgma_X.flatten())]
 
-        tbhdu_pro = fits.BinTableHDU.from_columns(fits.ColDefs(table_pro))
-        tbhdu_cov = fits.BinTableHDU.from_columns(fits.ColDefs(table_cov))
+        #tbhdu_pro = fits.BinTableHDU.from_columns(fits.ColDefs(table_pro))
+        #tbhdu_cov = fits.BinTableHDU.from_columns(fits.ColDefs(table_cov))
+        tbhdu_p = fits.BinTableHDU.from_columns(fits.ColDefs(table_p))
         
         
         primary_hdu = fits.PrimaryHDU(header=h)
         
-        hdul = fits.HDUList([primary_hdu, tbhdu_pro, tbhdu_cov])
+        hdul = fits.HDUList([primary_hdu, tbhdu_p])
         
         hdul.writeto(f'../{output_file}',overwrite=True)
                 
