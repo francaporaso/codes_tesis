83ed31e174c50a0f4608e3c313e98c47db1403f9
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2023-04-03 01:00:37 -0300

    añado diferencia en DS y S

--- fit_voids_MCMC.py
+++ fit_voids_MCMC.py
@@ -49,7 +49,7 @@ pos = np.array([np.random.uniform(.5,1.5,15),
 nwalkers, ndim = pos.shape
 
 # running emcee
-def run_emcee(RIN,ROUT):
+def fit_sigma(RIN,ROUT):
 
     profile = fits.open('../profiles/voids/Rv_15-18/Rv1518.fits')
     p = profile[1].data
@@ -57,10 +57,6 @@ def run_emcee(RIN,ROUT):
 
     maskr   = (p.Rp > (RIN))*(p.Rp < (ROUT))
     mr = np.meshgrid(maskr,maskr)[1]*np.meshgrid(maskr,maskr)[0]
-
-    CovDS  = cov.cov_DSt.reshape(len(p.Rp),len(p.Rp))[mr]
-    CovDS  = CovDS.reshape(sum(maskr),sum(maskr))
-    iCds   =  np.linalg.inv(CovDS)
 
     CovS  = cov.cov_S.reshape(len(p.Rp),len(p.Rp))[mr]
     CovS  = CovS.reshape(sum(maskr),sum(maskr))
@@ -70,28 +66,11 @@ def run_emcee(RIN,ROUT):
 
     t1 = time.time()
 
-    #Delta Sigma
-    samplerDS = emcee.EnsembleSampler(nwalkers, ndim, log_probabilityDS, args=(p.Rp,p.DSigmaT,iCds))
-    samplerDS.run_mcmc(pos, nit, progress=True)
     #Sigma
+    print('Fitting Sigma')
     samplerS = emcee.EnsembleSampler(nwalkers, ndim, log_probabilityS, args=(p.Rp,p.Sigma,iCs))
     samplerS.run_mcmc(pos, nit, progress=True)
 
-    # saving mcmc out
-    #Delta Sigma
-    mcmc_outDS = samplerDS.get_chain(flat=True).T
-
-    RvDS = np.percentile(mcmc_outDS[0][1500:], [16, 50, 84])
-    A0DS = np.percentile(mcmc_outDS[1][1500:], [16, 50, 84])
-    A3DS = np.percentile(mcmc_outDS[2][1500:], [16, 50, 84])
-    
-    tableDS = [fits.Column(name='RvDS', format='D', array=mcmc_outDS[0]),
-               fits.Column(name='A0DS', format='D', array=mcmc_outDS[1]),
-               fits.Column(name='A3DS', format='D', array=mcmc_outDS[2])]
-
-    tbhduDS = fits.BinTableHDU.from_columns(fits.ColDefs(tableDS))
-
-    #Sigma
     mcmc_outS = samplerS.get_chain(flat=True).T
 
     RvS = np.percentile(mcmc_outS[0][1500:], [16, 50, 84])
@@ -105,17 +84,6 @@ def run_emcee(RIN,ROUT):
     tbhduS = fits.BinTableHDU.from_columns(fits.ColDefs(tableS))
 
     h = fits.Header()
-    h.append('DS')
-    h.append(('Rv',np.round(RvDS[1],4)))
-    h.append(('eRv_min',np.round(np.diff(RvDS)[0],4)))
-    h.append(('eRv_max',np.round(np.diff(RvDS)[1],4)))
-    h.append(('A0',np.round(A0DS[1],4)))
-    h.append(('eA0_min',np.round(np.diff(A0DS)[0],4)))
-    h.append(('eA0_max',np.round(np.diff(A0DS)[1],4)))
-    h.append(('A3',np.round(A3DS[1],4)))
-    h.append(('eA3_min',np.round(np.diff(A3DS)[0],4)))
-    h.append(('eA3_max',np.round(np.diff(A3DS)[1],4)))
-
     h.append('S')
     h.append(('Rv',np.round(RvS[1],4)))
     h.append(('eRv_min',np.round(np.diff(RvS)[0],4)))
@@ -129,12 +97,83 @@ def run_emcee(RIN,ROUT):
     
     primary_hdu = fits.PrimaryHDU(header=h)
 
-    hdul = fits.HDUList([primary_hdu, tbhduDS, tbhduS])
-    hdul.writeto(folder+outfile,overwrite=True)
+    hdul = fits.HDUList([primary_hdu, tbhduS])
+    hdul.writeto(folder+outfile+f'S.fits',overwrite=True)
 
-    print('SAVED FILE')
+    print('SAVED FILE SIGMA')
+
+def fit_Dsigma(RIN,ROUT):
+
+    profile = fits.open('../profiles/voids/Rv_15-18/Rv1518.fits')
+    p = profile[1].data
+    cov = profile[2].data
+
+    maskr   = (p.Rp > (RIN))*(p.Rp < (ROUT))
+    mr = np.meshgrid(maskr,maskr)[1]*np.meshgrid(maskr,maskr)[0]
+    
+    CovDS  = cov.cov_DSt.reshape(len(p.Rp),len(p.Rp))[mr]
+    CovDS  = CovDS.reshape(sum(maskr),sum(maskr))
+    iCds   =  np.linalg.inv(CovDS)
+
+    p  = p[maskr]
+
+    t1 = time.time()
+
+    #Delta Sigma
+    print('Fitting DSigmaT')
+    samplerDS = emcee.EnsembleSampler(nwalkers, ndim, log_probabilityDS, args=(p.Rp,p.DSigmaT,iCds))
+    samplerDS.run_mcmc(pos, nit, progress=True)
+
+    mcmc_outDS = samplerDS.get_chain(flat=True).T
+
+    RvDS = np.percentile(mcmc_outDS[0][1500:], [16, 50, 84])
+    A0DS = np.percentile(mcmc_outDS[1][1500:], [16, 50, 84])
+    A3DS = np.percentile(mcmc_outDS[2][1500:], [16, 50, 84])
+    
+    tableDS = [fits.Column(name='RvDS', format='D', array=mcmc_outDS[0]),
+               fits.Column(name='A0DS', format='D', array=mcmc_outDS[1]),
+               fits.Column(name='A3DS', format='D', array=mcmc_outDS[2])]
+
+    tbhduDS = fits.BinTableHDU.from_columns(fits.ColDefs(tableDS))
+
+    h = fits.Header()
+    h.append('DS')
+    h.append(('Rv',np.round(RvDS[1],4)))
+    h.append(('eRv_min',np.round(np.diff(RvDS)[0],4)))
+    h.append(('eRv_max',np.round(np.diff(RvDS)[1],4)))
+    h.append(('A0',np.round(A0DS[1],4)))
+    h.append(('eA0_min',np.round(np.diff(A0DS)[0],4)))
+    h.append(('eA0_max',np.round(np.diff(A0DS)[1],4)))
+    h.append(('A3',np.round(A3DS[1],4)))
+    h.append(('eA3_min',np.round(np.diff(A3DS)[0],4)))
+    h.append(('eA3_max',np.round(np.diff(A3DS)[1],4)))
+
+    primary_hdu = fits.PrimaryHDU(header=h)
+
+    hdul = fits.HDUList([primary_hdu, tbhduDS])
+    hdul.writeto(folder+outfile+f'DS.fits',overwrite=True)
+
+    print('SAVED FILE DELTA SIGMA')
+
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-fS', action='store', dest='fS',default='False')
+    parser.add_argument('-fDS', action='store', dest='fDS',default='False')
+
+    args = parser.parse_args()
+
+    if args.fS == 'True':
+        fS = True
+    else:
+        fS = False
+
+    if args.fDS == 'True':
+        fDS = True
+    else:
+        fDS = False
+
     profile = fits.open('../profiles/voids/Rv_15-18/Rv1518.fits')
     p = profile[1].data
     cov = profile[2].data
@@ -151,6 +190,12 @@ if __name__ == '__main__':
         pass
 
     folder = f'../profiles/voids/fitting'
-    outfile = f'fitMCMC_Rv{int(Rv_min)}{int(Rv_max)}.fits'
+    outfile = f'fitMCMC_Rv{int(Rv_min)}{int(Rv_max)}'
 
-    run_emcee(RIN,ROUT)
+    if fS:
+        fit_sigma(RIN,ROUT)
+    elif fDS:
+        fit_sigma(RIN,ROUT)
+    else:
+        fit_sigma(RIN,ROUT)
+        fit_Dsigma(RIN,ROUT)
