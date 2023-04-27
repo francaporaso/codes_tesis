e6960b0d627b8a288e674bd2e2ec94634781db9b
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2023-04-27 15:48:46 -0300

    perfiles 3d con mice

---
+++ perfiles3d.py
@@ -0,0 +1,18 @@
+#tenemos el centro, queremos los halos en una esfera cetrada ahi y hacer el perfil 3D
+
+#una forma facil es pedir que sqrrt(x²+y²+z²) este entre R1 y R2 -> readaptar para quedarse con
+#las galaxias en el cubo -> en vez de ra,dec,reds en xyz y cuente halos en cascarones esfericos
+#para sacar una dist de densidad
+
+# rho = masa(r1<r<r2)/V_cascaron
+
+#Masa = # particulas*masa_particula 
+
+# #particulas = \sum n_part_en_cada_halo
+
+#necesitamos si es central o satelite, pos del halo, masa del halo
+
+#vamos sumando masa de halos en cada region
+#-> Masa = \sum masa_halo
+
+#para sumar una sola vez vamos sumando los halos de galaxias centrales 
