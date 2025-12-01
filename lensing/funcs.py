e1019eac84610fb422eba87fd63e189ecd2b1f5a
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2025-12-01 17:57:36 -0300

    updating lensing profile code from modified_gravity/

---
+++ lensing/funcs.py
@@ -0,0 +1,147 @@
+#--------------------------- Functions --------------------------------------------
+import numpy as np
+from astropy.coordinates import angular_separation, position_angle
+#from astropy.constants import G,c,M_sun,pc
+#from astropy.io import fits
+from astropy.table import Table
+import healpy as hp
+#parameters
+# cvel = c.value;    # Speed of light (m.s-1)
+# G    = G.value;    # Gravitational constant (m3.kg-1.s-2)
+# pc   = pc.value    # 1 pc (m)
+# Msun = M_sun.value # Solar mass (kg)
+
+def cov_matrix(array):
+        
+    K = len(array)
+    Kmean = np.average(array,axis=0)
+    bins = array.shape[1]
+    
+    COV = np.zeros((bins,bins))
+    
+    for k in range(K):
+        dif = (array[k]- Kmean)
+        COV += np.outer(dif,dif)        
+    
+    COV *= (K-1)/K
+    return COV
+
+def normalize_cov(cov):
+    norm_cov = np.zeros_like(cov)
+    for i in range(len(cov)):
+        for j in range(len(cov)):
+            norm_cov[i,j]=cov[i,j]/np.sqrt(cov[i,i]*cov[j,j])
+    return norm_cov
+
+def eq2p2(ra_gal, dec_gal, RA0,DEC0):
+    """
+    angular separation and position angle from centre (RA0,DEC0) to gal position (ra_gal, dec_gal)
+    returns two angles in radians
+    all parameters must be in radians.
+    """
+
+    ra_prime = ra_gal - RA0
+
+    rad = angular_separation(ra_prime, dec_gal, 0.0, DEC0)
+    theta = position_angle(0.0, DEC0, ra_prime, dec_gal).value
+
+    return rad, theta
+
+def jackknife_equalarea(ramin, ramax, decmin, decmax):
+    pass
+
+def lenscat_load(name,
+                 Rv_min, Rv_max, z_min, z_max, delta_min, delta_max, rho1_min=-1.0, rho1_max=0.0, flag=2,
+                 NCHUNKS:int=1, NK:int=1, octant=False, MICE=False, fullshape=True):
+
+    if MICE:
+        RV,RA,DEC,Z,R1,R2 = 1,2,3,4,8,9
+    else:
+        RV,RA,DEC,Z,R1,R2 = 0,1,2,3,7,8
+    ## 0:Rv, 1:ra, 2:dec, 3:z, 4:xv, 5:yv, 6:zv, 7:rho1, 8:rho2, 9:logp, 10:diff CdM y CdV, 11:flag
+    ## CdM: centro de masa
+    ## CdV: centro del void
+    try: 
+        L = np.loadtxt("/home/fcaporaso/cats/L768/"+name, dtype='f4').T
+    except:
+        L = np.loadtxt(name, dtype='f4').T
+    
+    if octant: 
+        print(' Using octant '.center(40,'#'), flush=True)
+        # selecciono los void en un octante
+        eps = 6.0 ## sale de tomar el angulo substendido por el void más grande al redshift más bajo
+        L = L[:, (L[RA] >= 0.0+eps) & (L[RA] <= 90.0-eps) & (L[DEC]>= 0.0+eps) & (L[DEC] <= 90.0-eps)]
+
+    sqrt_NK = int(np.sqrt(NK))
+    NNN = len(L[0]) ##total number of voids
+    ra,dec = L[RA],L[DEC]
+    K    = np.zeros((NK+1,NNN), dtype=bool)
+    K[0] = np.ones(NNN, dtype=bool)
+
+    ramin  = np.min(ra)
+    cdec   = np.sin(np.deg2rad(dec))
+    decmin = np.min(cdec)
+    dra    = ((np.max(ra)+1.e-5) - ramin)/sqrt_NK
+    ddec   = ((np.max(cdec)+1.e-5) - decmin)/sqrt_NK
+
+    c = 1
+    for a in range(sqrt_NK): 
+        for d in range(sqrt_NK): 
+            mra  = (ra  >= ramin + a*dra)&(ra < ramin + (a+1)*dra) 
+            mdec = (cdec >= decmin + d*ddec)&(cdec < decmin + (d+1)*ddec) 
+            K[c] = ~(mra&mdec)
+            c += 1
+
+    mask = (L[RV] >= Rv_min) & (L[RV] < Rv_max) & (L[Z] >= z_min) & (L[Z] < z_max) & (
+            L[R1] >= rho1_min) & (L[R1] < rho1_max) & (L[R2] >= delta_min) & (L[R2] < delta_max) & (L[11] >= flag)
+
+    nvoids = mask.sum()
+    if fullshape:
+        L = L[:, mask]
+    else:
+        L = L[[RV,RA,DEC,Z]][:, mask]
+
+    if bool(NCHUNKS-1):
+        if NCHUNKS > nvoids:
+            NCHUNKS = nvoids
+        lbins = round(nvoids/NCHUNKS)
+        slices = (np.arange(lbins)+1)*NCHUNKS
+        slices = slices[(slices < nvoids)]
+        L = np.split(L.T, slices)
+        K = np.split(K.T, slices)
+
+    return L, K, nvoids
+
+def sourcecat_load(name='MICE_sources_HSN_withextra.fits', nback=30.0, seed=0):
+    
+    folder = '/mnt/simulations/MICE/'
+    S = Table.read(folder+name, memmap=True, format='fits')
+    
+    # nback :: number density of background sources [arcsec^-2] 
+    if nback<=30.0:
+        rng = np.random.default_rng(seed)
+        # octant surface = 5157.0 deg^2
+        n_select = int(nback*5157.0*3600.0)
+        j = rng.choice(range(len(S)), n_select, dtype=bool)
+        return S[j]
+    
+    return S
+
+
+# ## Cuentas en drive 'IATE/sphere_plane_cut.pdf'
+# def get_masked_data_intersection(psi, ra0, dec0, z0):
+#     '''
+#     objects are selected by intersecting the sphere with a plane
+#     and keeping those inside the spherical cap.
+#     '''
+
+#     ra0_rad = np.deg2rad(ra0)
+#     dec0_rad = np.deg2rad(dec0)
+#     cos_dec0 = np.cos(dec0_rad)
+
+#     mask_z = _S['true_redshift_gal']>z0+0.1
+#     mask_field = (cos_dec0*np.cos(ra0_rad)*_S['cos_dec_gal']*_S['cos_ra_gal']
+#                 + cos_dec0*np.sin(ra0_rad)*_S['cos_dec_gal']*_S['sin_ra_gal'] 
+#                 + np.sin(dec0_rad)*_S['sin_dec_gal'] >= np.sqrt(1-np.sin(np.deg2rad(psi))**2))
+    
+#     return _S[mask_field&mask_z]
---
+++ lensing/makeconfig.py
@@ -0,0 +1,83 @@
+import numpy as np
+from argparse import ArgumentParser
+import toml
+
+parser = ArgumentParser()
+parser.add_argument('--dz', action='store', type=float, default=0.05)
+parser.add_argument('--z_min', action='store', type=float, default=0.1)
+parser.add_argument('--z_max', action='store', type=float, default=0.3)
+parser.add_argument('--nRv', action='store', type=int, default=1)
+parser.add_argument('--Rv_min', action='store', type=float, default=8.0)
+parser.add_argument('--Rv_max', action='store', type=float, default=30.0)
+parser.add_argument('--voidtype', action='store', type=str, default='mix', choices=['mix', 'S', 'R'])
+parser.add_argument('--delta_min', action='store', type=float, default=-1.0)
+parser.add_argument('--delta_max', action='store', type=float, default=10.0)
+parser.add_argument('--RIN', action='store', type=float, default=0.01)
+parser.add_argument('--ROUT', action='store', type=float, default=5.0)
+parser.add_argument('--NDOTS', action='store', type=int, default=40)
+parser.add_argument('--nback', action='store', type=str, default='FULL', choices=['FULL', 'DES', 'EUCLID'])
+args = parser.parse_args()
+
+# TODO: quiza mejor usar mediana... para eso debo cargar el catálogo...
+rvrange = np.linspace(args.Rv_min, args.Rv_max, args.nRv+1)
+radii = np.column_stack([rvrange[:-1], rvrange[1:]])
+
+nz = np.round((args.z_max-args.z_min)/args.dz).astype(int)+1
+zrange = np.linspace(args.z_min, args.z_max, nz, endpoint=True)
+redshift = np.column_stack([zrange[:-1], zrange[1:]])
+
+if args.voidtype==None:
+    delta = [(args.delta_min, args.delta_max)]
+elif args.voidtype=='mix':
+    delta = [(-1.0,0.0), (0.0,10.0)]
+elif args.voidtype=='S':
+    delta = [(0.0,10.0)]
+elif args.voidtype=='R':
+    delta = [(-1.0,0.0)]
+
+if args.nback=='DES':
+    nback = 6.0
+elif args.nback=='EUCLID':
+    nback = 26.0
+else:
+    nback = 31.0
+
+print(f'{radii=}')
+print(f'{redshift=}')
+print(f'{delta=}')
+
+
+i = 0
+for rv in radii:
+    for zs in redshift:
+        for d in delta:
+            
+            config = {
+                'NCORES':16,
+                'NK':100,
+                'BIN':'lin',
+                'prof':{
+                    'NDOTS':args.NDOTS,
+                    'RIN':args.RIN,
+                    'ROUT':args.ROUT,
+                },
+                'void': {
+                    'Rv_min':float(rv[0]),
+                    'Rv_max':float(rv[1]),
+                    'z_min':float(zs[0]),
+                    'z_max':float(zs[1]),
+                    'delta_min':float(d[0]),
+                    'delta_max':float(d[1])
+                },
+                'lens':{
+                    'name':'voids_MICE.dat',
+                },
+                'source':{
+                    'name':'MICE_sources_HSN_withextra.fits',
+                    'nback':nback
+                }    
+            }
+
+            with open(f'config_{i}.toml', 'w') as f:
+                toml.dump(config, f)
+            i+=1
