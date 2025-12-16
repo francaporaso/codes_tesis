8567efce970c2ee8d1e030a9a338a016894fac4f
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2025-12-16 14:26:11 -0300

    debug shapenoise: eps1 must be substracted from gamma1 (see ejg)

--- lensing/voidsMICE.py
+++ lensing/voidsMICE.py
@@ -137,7 +137,7 @@ def partial_profile(inp):
     e1 = catdata['gamma1']
     e2 = -catdata['gamma2']
     if _SHAPENOISE:
-        e1 += catdata['eps1']
+        e1 -= catdata['eps1']
         e2 += catdata['eps2']
 
     #get tangential ellipticities
