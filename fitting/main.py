090b39f24ffd16ec0dbadcd8f3c22b9e07c0df9b
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2026-05-27 15:42:55 -0300

    update: saving the fitted parameter

--- fitting/main.py
+++ fitting/main.py
@@ -47,25 +47,51 @@ if __name__ == '__main__':
     NIT = 5000
     NWALKERS = 64
 
-    data_filename = 'lensing/results/lensing_EUC-PURE_MICE_N30_Rv06-10_z020-040_typeS_binlin.fits'
-    chain_filename = 'fitting/results/fitting_EUC-PURE_MICE_N30_Rv06-10_z020-040_typeS_binlin.hdf5'
     model_name = 'HSW'
     observable = 'delta_sigma'
-    cov_mode = 'diag'
+    cov_mode = 'full'
 
-    sampler = run_emcee(
-        NCORES=NCORES,NIT=NIT,NWALKERS=NWALKERS,
-        data_filename=data_filename,
-        save_filename=chain_filename,
-        model_name=model_name,
-        observable=observable,
-        cov_mode=cov_mode,
-        init_guess=default_guess.get(model_name)
-    )
-    # TODO: que guarde los valores de mejor ajuste!
+    for i, rv in enumerate(['06-10', '10-50']):
+        for j, t in enumrerate(['mixed', 'S', 'R']):
 
-    plot_chains(sampler.get_chain())
-    plt.show()
+            data_filename = f'lensing/results/lensing_rev2_MICE_N30_Rv{rv}_z020-040_type{t}_binlin.fits'
+            chain_filename = f'fitting/results/fitting_MICE_rev2_N30_Rv{rv}_z020-040_type{t}_binlin.hdf5'
 
-    plot_corner(sampler);
-    plt.show()
+            sampler = run_emcee(
+                NCORES=NCORES,NIT=NIT,NWALKERS=NWALKERS,
+                data_filename=data_filename,
+                save_filename=chain_filename,
+                model_name=model_name,
+                observable=observable,
+                cov_mode=cov_mode,
+                init_guess=default_guess.get(model_name)
+            )
+
+            param_names = list(default_limits.get(model_name).keys())
+            # not possible to fix params for now
+            fitpar, errpar = get_fitted_params(
+                sampler.get_chain(discard=int(NIT*0.15)), 
+                param_names
+            )
+
+            with h5py.File(chain_filename, 'a') as f:
+                group_path = f'fitedparams/{model_name}/{cov_mode}'
+
+                # Overwrite if exists
+                if group_path in f:
+                    del f[group_path]
+
+                grp = f.create_group(group_path)
+
+                for pname in param_names:
+                    pgrp = grp.create_group(pname)
+                    pgrp.create_dataset('median', data=fitpar[pname])
+                    pgrp.create_dataset('errs', data=np.array(errpar[pname]))
+
+
+            if PLOT:
+                plot_chains(sampler.get_chain())
+                plt.show()
+
+                plot_corner(sampler);
+                plt.show()
