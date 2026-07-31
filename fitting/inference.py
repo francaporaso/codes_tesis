011a462771ee511669a8eea4dd10fd69c7e8ff41
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2026-07-31 12:22:37 -0300

    update: added the posibility to fix params, likelihood should be instantiated with fixed param dict {param: fixvalue}

--- fitting/inference.py
+++ fitting/inference.py
@@ -9,11 +9,10 @@ class Likelihood:
     }
 
     def __init__(
-        self, data, model, param_limits, observable="delta_sigma", cov_mode="full"
+        self, data, model, param_limits, observable, cov_mode, fixed_params=None
     ):
 
         self.R = data.R
-        self.limits = param_limits
         # self.hartlap_factor = (data.Njk-len(self.R)-2)/(data.Njk-1)
         self.hartlap_factor = 1
 
@@ -25,7 +24,6 @@ class Likelihood:
             )
 
         self.ydata = getattr(data, data_attr)
-
         self.cov = getattr(data, cov_attr)
         self.func = getattr(model, func_name)
 
@@ -38,11 +36,23 @@ class Likelihood:
         else:
             raise ValueError('cov_mode must be either "full" or "diag"')
 
-        self.param_name = list(self.limits.keys())
+        all_params = model.params[observable]
+        fixed_params = fixed_params or {}
+        self.param_name = [p for p in all_params if p not in fixed_params]
+        self.limits = {k: param_limits[k] for k in self.param_name}
         self.nparams = len(self.param_name)
 
+        # needed if there are fixed params...
+        # creates a template for where to fill free params draw
+        self._template = np.empty(len(all_params))
+        self._freeidx = np.array([all_params.index(p) for p in self.param_name])
+        for p, v in fixed_params.items():
+            self._template[all_params.index(p)] = v
+
     def log_likelihood(self, theta):
-        model = self.func(self.R, *theta)  # *self.rhomean
+        full = self._template.copy()
+        full[self._freeidx] = theta
+        model = self.func(self.R, *full)  # *self.rhomean
         dist = self.ydata - model
         return -0.5 * np.dot(dist, np.dot(self.yerr, dist))
 
